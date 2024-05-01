c158ec2c2905e30d2973f6853d6118b1f28fd99d
Royal Finch <Royal2791@gmail.com>
2024-05-01 16:39:12 -0400

    first pass on styling

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -6,7 +6,7 @@ function Order({ plan, price }) {
       <img className="order__image" src="images/icon-music.svg" alt="" />
       <div className="order__details">
         <p className="details__plan">{plan} Plan</p>
-        <p className="details__price">{`${price}/${frequency[plan]}`}</p>
+        <p className="details__price">{`$${price}/${frequency[plan]}`}</p>
       </div>
       <a className="order__change" href="">
         Change
--- src/components/OrderSummary.jsx
+++ src/components/OrderSummary.jsx
@@ -3,18 +3,24 @@ import Order from './Order';
 function OrderSummary() {
   return (
     <div className="order-summary">
-      <div className="order-summary__image">
-        <img src="images/illustration-hero.svg" alt="" />
+      <div className="order-summary__img-section">
+        <img
+          className="order-summary__img"
+          src="images/illustration-hero.svg"
+          alt=""
+        />
       </div>
-      <div className="order-summary-details">
-        <h1>Order Summary</h1>
-        <p>
+      <div className="order-summary__info">
+        <h1 className="order-summary__heading">Order Summary</h1>
+        <p className="order-summary__description">
           You can now listen to millions of songs, audiobooks, and podcasts on
           any device anywhere you like!
         </p>
         <Order plan="Annual" price="59.99" />
-        <a href="">Proceed to Payment</a>
-        <button>Cancel Order</button>
+        <a className="order-summary__btn-proceed" href="">
+          Proceed to Payment
+        </a>
+        <button className="order-summary__btn-cancel">Cancel Order</button>
       </div>
     </div>
   );
